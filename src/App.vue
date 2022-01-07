d8f7cbe5c2115134df388397784e27f60358d22e
Rodrigo Segatto <segattorodrigo@hotmail.com>
2022-01-07 07:46:44 -0300

    Menu SPA e Routes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="corpo">
-    
+    <meu-menu :routes="routes" />
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+  import {routes} from './routes';
+  import Menu from './components/shared/menu/Menu.vue';
   export default {
-    
+    data () {
+      return {
+        routes
+      }
+    },
+    components: {
+      'meu-menu': Menu
+    }
   }
 </script>
 
@@ -15,5 +27,13 @@
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .4s
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import {routes} from './routes';
 
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
